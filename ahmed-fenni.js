f513c0426ad37e5c0682e763a685b5d28b64c8b4
ahmed-fenni <ahmedfenny8@gmail.com>
2020-03-12 16:52:30 +0100

    solved

---
+++ ahmed-fenni.js
@@ -0,0 +1,28 @@
+# introduction-to-git
+first steps into git ...
+
+Instructions:
+Fork and clone this repo. From your fork, provide answers to questions below. When you have answered all the questions, commit and push to your repository.
+
+1-Who is your favorite super hero? 
+  batman
+
+2-What is one interesting thing about you? 
+   i m so smart 
+3-What animal is your spirit animal? 
+     eagle
+4-What is your favorite food? 
+      mloukheya
+5-What is your favorite beverage? 
+       juice 
+6-What is your favorite national park or landmark? 
+        ayam carthage
+7-What is your favorite movie? 
+        remember me
+Advanced Content:
+From your fork, make a new branch. On the new branch, write a new question and answer under the questions already provided. Commit the changes and merge the new branch with the master branch.
+
+Resources:
+https://learngitbranching.js.org/
+ 8 where do you live 
+  ouardia
